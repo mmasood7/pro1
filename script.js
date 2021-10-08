b922e777ac0bfc3d096c4d0c60ecae6da37918ef
mmasood7 <89464240+mmasood7@users.noreply.github.com>
2021-10-09 00:39:16 +0500

    opp k sara concepts

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
 "use strict";
-const a = 5;
-const arr = [
-  [a],
-  [a + 1, a - 1],
-  [a, a + 4, a - 4],
-  [a - 2, a - 4, a + 2, a + 4],
-  [a, a - 2, a + 2, a - 4, a + 4],
-  [a - 1, a - 2, a - 4, a + 1, a + 2, a + 4],
-];
-const rollDice = function () {
-  const allTd = document.querySelectorAll("td");
-  allTd.forEach((item) => item.classList.remove("dot"));
-  const randomNum = function () {
-    return Math.floor(Math.random() * 6);
-  };
-  const randomPostition = function () {
-    return Math.floor(Math.random() * 90);
-  };
-  let num = randomNum();
-  console.log(num);
-  arr.forEach((item, i) => {
-    if (num === 6) {
-      document.querySelector("table").style.backgroundColor = "green";
-    }
-    if (num === i) {
-      item.forEach((item) => {
-        document.querySelector("table").style.transition = `2s`;
-        document.getElementById(`no${item}`).classList.add("dot");
-        // document.querySelector(".dot").style.backgroundColor = `#${
-        //   Math.floor(Math.random() * 600) + 50
-        // }`;
+class Student {
+  name = "";
+  email = "";
+  password = "";
+}
+const students = [];
+const userNameEl = document.querySelector("#userName");
+const userEmailEl = document.querySelector("#userEmail");
+const userPassEl = document.querySelector("#userPass");
+const subForm = document.querySelector("#myForm");
+
+const createNewStudentRecord = function (e) {
+  e.preventDefault();
+  if (userEmailEl.value && userNameEl && userPassEl.value) {
+    const s1 = new Student();
+    s1.name = userNameEl.value;
+    s1.email = userEmailEl.value;
+    s1.password = userPassEl.value;
+    if (students.length !== 0) {
+      students.find((student) => {
+        if (
+          student.name.toLowerCase.trim === userNameEl.value.toLowerCase.trim
+        ) {
+          students.push(s1);
+        } else {
+          alert(`${s1.name} User Already Existes`);
+          return;
+        }
       });
-      document.querySelector(
-        "table"
-      ).style.transform = `rotate(${randomPostition()}deg)`;
-      document.querySelector("table").style.boxShadow = `${
-        Math.floor(Math.random() * 3) + 5
-      }px ${Math.floor(Math.random() * 15) + 10}px`;
+    } else {
+      students.push(s1);
     }
-  });
-  const pos = ["top", "bottom", "right", "left"];
-  const a = pos[Math.floor(Math.random() * 4)];
-  document.querySelector("table").style.top = `${
-    Math.floor(Math.random() * 200) + 50
-  }px`;
-  document.querySelector("table").style.left = `${
-    Math.floor(Math.random() * 600) + 50
-  }px`;
+  }
 };
-// document.getElementById("table").addEventListener("click", rollDice);
+subForm.addEventListener("submit", (event) => {
+  createNewStudentRecord(event);
+});
